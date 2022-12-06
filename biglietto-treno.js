bd0865defbab01c6e80e82de35b819190d0b95f3
AndreaChiari <chiariandrea94@gmail.com>
2022-12-06 16:49:41 +0100

    update

--- biglietto-treno.js
+++ biglietto-treno.js
@@ -1,1 +1,36 @@
 console.log('JS OK')
+
+/*
+1) chiedo all'utente il numero di chilometri che vuole percorrere
+2) chiedo all'utente l'eta'
+3) definisco la variabile del costo di 0.21 euro al KM 
+4) calcolo il costo totale del biglietto con il costo al KM
+5) applico la variabile del 20% di sconto al prezzo se l'individuo e' minorenne
+6) applico la variabile del 40% di sconto al prezzo se l'individuo e' maggiore di 65 anni
+7) controllo in console il prezzo finale espresso in due decimali.
+*/
+
+
+
+//1) prendo gli elementi dalla pagina
+const Km = document.getElementById('Km');
+const age = document.getElementById('age');
+const button = document.getElementById("button");
+console.log('Km,button,age');
+
+
+button.addEventListener('click', function (){
+    
+    const kmValue = Km.value;
+    const ageValue = age.value;
+    console.log(kmValue,ageValue);
+    
+});
+
+
+
+
+
+
+
+
